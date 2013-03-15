feeea5800bc22ec95100a4d8cd1fd46f06108c51
Mark Maglana <mmaglana@gmail.com>
2013-03-15 00:13:51 -0700

    Add helper methods to test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,17 @@ require "bundler/setup"
 require "minitest/autorun"
 require "minitest/colorize"
 require "manamana"
+
+def get_or_create_tmp_dir
+  root_dir = get_root_dir
+  Dir.mkdir 'tmp' unless root_dir.entries.include? 'tmp'
+  Dir.chdir 'tmp'
+  Dir.new(Dir.pwd)  
+end
+
+def get_root_dir
+  unless Dir.new(Dir.pwd).entries.include? 'manamana.gemspec'
+    Dir.chdir '..'
+  end
+  Dir.new(Dir.pwd)  
+end
