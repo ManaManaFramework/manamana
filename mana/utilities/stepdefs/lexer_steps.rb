8eb870a927e35e01e7f327e62735338610dff0fa
Mark Maglana <mmaglana@gmail.com>
2013-09-13 13:19:37 -0700

    First Implementation of ManaMana testing itself

---
+++ mana/utilities/stepdefs/lexer_steps.rb
@@ -0,0 +1,11 @@
+# Add the gem's lib folder to the load path
+$:.unshift File.expand_path("../../../../lib", __FILE__)
+require 'manamana/rdsl/lexer'
+
+step /^Tokenized ([\w\W]+) must equal (.+)$/ do |code_block, tokens_str|
+
+  output = ManaMana::RDSL::Lexer.new.tokenize(code_block)
+  tokens = eval tokens_str
+  output.must_equal tokens
+
+end
